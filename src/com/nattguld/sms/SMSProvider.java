f44866d5e583b869bfc2a325f02d0b712a73f2da
Nattguld <6783744+randqm@users.noreply.github.com>
2019-06-12 19:47:12 +0200

    Library update

--- src/com/nattguld/sms/SMSProvider.java
+++ src/com/nattguld/sms/SMSProvider.java
@@ -1,6 +1,6 @@
 package com.nattguld.sms;
 
-import com.nattguld.util.generics.kvps.impl.BasicNameValuePair;
+import com.nattguld.util.generics.kvps.impl.ObjectKeyValuePair;
 
 /**
  * 
@@ -10,99 +10,99 @@ import com.nattguld.util.generics.kvps.impl.BasicNameValuePair;
 
 public enum SMSProvider {
 	
-	GETSMSCODE("GetSMSCode", "http://www.getsmscode.com/do.php", true, true, new BasicNameValuePair[] {
-			new BasicNameValuePair(Platform.GOOGLE, "1"),
-			new BasicNameValuePair(Platform.FACEBOOK, "2"),
-			new BasicNameValuePair(Platform.WHATSAPP, "3"),
-			new BasicNameValuePair(Platform.CHATROULETTE, "6"),
-			new BasicNameValuePair(Platform.EBAY, "7"),
-			new BasicNameValuePair(Platform.INSTAGRAM, "8"),
-			new BasicNameValuePair(Platform.TELEGRAM, "10"),
-			new BasicNameValuePair(Platform.YAHOO, "15"),
-			new BasicNameValuePair(Platform.TINDER, "51"),
-			new BasicNameValuePair(Platform.VIBER, "55"),
-			new BasicNameValuePair(Platform.TWITTER, "66"),
-			new BasicNameValuePair(Platform.SKYPE, "114"),
-			new BasicNameValuePair(Platform.AMAZON, "146"),
-			new BasicNameValuePair(Platform.TWILIO, "248"),
-			new BasicNameValuePair(Platform.BADOO, "283"),
-			new BasicNameValuePair(Platform.SNAPCHAT, "287"),
-			new BasicNameValuePair(Platform.WEEBLY, "297"),
-			new BasicNameValuePair(Platform.MICROSOFT, "338"),
-			new BasicNameValuePair(Platform.MEETME, "344"),
-			new BasicNameValuePair(Platform.YANDEX, "346"),
-			new BasicNameValuePair(Platform.FIVERR, "356"),
-			new BasicNameValuePair(Platform.ALIBABA, "387"),
-			new BasicNameValuePair(Platform.PROTONMAIL, "404"),
-			new BasicNameValuePair(Platform.STEAM, "427"),
-			new BasicNameValuePair(Platform.CRAIGSLIST, "469"),
-			new BasicNameValuePair(Platform.G2A, "476"),
-			new BasicNameValuePair(Platform.MAILRU, "489"),
-			new BasicNameValuePair(Platform.ZOOSK, "496"),
-			new BasicNameValuePair(Platform.MUSICALLY, "501"),
-			new BasicNameValuePair(Platform.LINKEDIN, "509"),
-			new BasicNameValuePair(Platform.DISCORD, "544"),
-			new BasicNameValuePair(Platform.SPOTIFY, "545"),
-			new BasicNameValuePair(Platform.REDIFFMAIL, "616"),
-			new BasicNameValuePair(Platform.NETFLIX, "622"),
-			new BasicNameValuePair(Platform.APPLE, "644"),
-			new BasicNameValuePair(Platform.BUMBLE, "646"),
-			new BasicNameValuePair(Platform.OTHER, "ot"),
-			new BasicNameValuePair(Platform.OTHER, "ot"),
-			new BasicNameValuePair(Platform.OTHER, "ot"),
-			new BasicNameValuePair(Platform.OTHER, "ot"),
+	GETSMSCODE("GetSMSCode", "http://www.getsmscode.com/do.php", true, true, new ObjectKeyValuePair[] {
+			new ObjectKeyValuePair(Platform.GOOGLE, "1"),
+			new ObjectKeyValuePair(Platform.FACEBOOK, "2"),
+			new ObjectKeyValuePair(Platform.WHATSAPP, "3"),
+			new ObjectKeyValuePair(Platform.CHATROULETTE, "6"),
+			new ObjectKeyValuePair(Platform.EBAY, "7"),
+			new ObjectKeyValuePair(Platform.INSTAGRAM, "8"),
+			new ObjectKeyValuePair(Platform.TELEGRAM, "10"),
+			new ObjectKeyValuePair(Platform.YAHOO, "15"),
+			new ObjectKeyValuePair(Platform.TINDER, "51"),
+			new ObjectKeyValuePair(Platform.VIBER, "55"),
+			new ObjectKeyValuePair(Platform.TWITTER, "66"),
+			new ObjectKeyValuePair(Platform.SKYPE, "114"),
+			new ObjectKeyValuePair(Platform.AMAZON, "146"),
+			new ObjectKeyValuePair(Platform.TWILIO, "248"),
+			new ObjectKeyValuePair(Platform.BADOO, "283"),
+			new ObjectKeyValuePair(Platform.SNAPCHAT, "287"),
+			new ObjectKeyValuePair(Platform.WEEBLY, "297"),
+			new ObjectKeyValuePair(Platform.MICROSOFT, "338"),
+			new ObjectKeyValuePair(Platform.MEETME, "344"),
+			new ObjectKeyValuePair(Platform.YANDEX, "346"),
+			new ObjectKeyValuePair(Platform.FIVERR, "356"),
+			new ObjectKeyValuePair(Platform.ALIBABA, "387"),
+			new ObjectKeyValuePair(Platform.PROTONMAIL, "404"),
+			new ObjectKeyValuePair(Platform.STEAM, "427"),
+			new ObjectKeyValuePair(Platform.CRAIGSLIST, "469"),
+			new ObjectKeyValuePair(Platform.G2A, "476"),
+			new ObjectKeyValuePair(Platform.MAILRU, "489"),
+			new ObjectKeyValuePair(Platform.ZOOSK, "496"),
+			new ObjectKeyValuePair(Platform.MUSICALLY, "501"),
+			new ObjectKeyValuePair(Platform.LINKEDIN, "509"),
+			new ObjectKeyValuePair(Platform.DISCORD, "544"),
+			new ObjectKeyValuePair(Platform.SPOTIFY, "545"),
+			new ObjectKeyValuePair(Platform.REDIFFMAIL, "616"),
+			new ObjectKeyValuePair(Platform.NETFLIX, "622"),
+			new ObjectKeyValuePair(Platform.APPLE, "644"),
+			new ObjectKeyValuePair(Platform.BUMBLE, "646"),
+			new ObjectKeyValuePair(Platform.OTHER, "ot"),
+			new ObjectKeyValuePair(Platform.OTHER, "ot"),
+			new ObjectKeyValuePair(Platform.OTHER, "ot"),
+			new ObjectKeyValuePair(Platform.OTHER, "ot"),
 	}),
-	SMSPVA("SMSPVA", "http://smspva.com/priemnik.php", false, true, new BasicNameValuePair[] {
-			new BasicNameValuePair(Platform.AMAZON, "opt44"),
-			new BasicNameValuePair(Platform.BADOO, "opt56"),
-			new BasicNameValuePair(Platform.BURGERKING, "opt3"),
-			new BasicNameValuePair(Platform.CRAIGSLIST, "opt26"),
-			new BasicNameValuePair(Platform.DISCORD, "opt45"),
-			new BasicNameValuePair(Platform.FACEBOOK, "opt2"),
-			new BasicNameValuePair(Platform.FASTMAIL, "opt43"),
-			new BasicNameValuePair(Platform.FIVERR, "opt6"),
-			new BasicNameValuePair(Platform.G2A, "opt68"),
-			new BasicNameValuePair(Platform.GOOGLE, "opt1"),
-			new BasicNameValuePair(Platform.INSTAGRAM, "opt16"),
-			new BasicNameValuePair(Platform.LINKEDIN, "opt8"),
-			new BasicNameValuePair(Platform.MAILRU, "opt33"),
-			new BasicNameValuePair(Platform.MEETME, "opt17"),
-			new BasicNameValuePair(Platform.MICROSOFT, "opt15"),
-			new BasicNameValuePair(Platform.NETFLIX, "opt101"),
-			new BasicNameValuePair(Platform.OTHER, "opt19"),
-			new BasicNameValuePair(Platform.PAYPAL, "opt83"),
-			new BasicNameValuePair(Platform.EBAY, "opt83"),
-			new BasicNameValuePair(Platform.PROTONMAIL, "opt57"),
-			new BasicNameValuePair(Platform.SNAPCHAT, "opt90"),
-			new BasicNameValuePair(Platform.SPOTIFY, "opt98"),
-			new BasicNameValuePair(Platform.STEAM, "opt58"),
-			new BasicNameValuePair(Platform.TELEGRAM, "opt29"),
-			new BasicNameValuePair(Platform.TINDER, "opt9"),
-			new BasicNameValuePair(Platform.TWILIO, "opt66"),
-			new BasicNameValuePair(Platform.TWITTER, "opt41"),
-			new BasicNameValuePair(Platform.VIBER, "opt11"),
-			new BasicNameValuePair(Platform.WEEBLY, "opt54"),
-			new BasicNameValuePair(Platform.WHATSAPP, "opt20"),
-			new BasicNameValuePair(Platform.YAHOO, "opt65"),
-			new BasicNameValuePair(Platform.YANDEX, "opt23"),
+	SMSPVA("SMSPVA", "http://smspva.com/priemnik.php", false, true, new ObjectKeyValuePair[] {
+			new ObjectKeyValuePair(Platform.AMAZON, "opt44"),
+			new ObjectKeyValuePair(Platform.BADOO, "opt56"),
+			new ObjectKeyValuePair(Platform.BURGERKING, "opt3"),
+			new ObjectKeyValuePair(Platform.CRAIGSLIST, "opt26"),
+			new ObjectKeyValuePair(Platform.DISCORD, "opt45"),
+			new ObjectKeyValuePair(Platform.FACEBOOK, "opt2"),
+			new ObjectKeyValuePair(Platform.FASTMAIL, "opt43"),
+			new ObjectKeyValuePair(Platform.FIVERR, "opt6"),
+			new ObjectKeyValuePair(Platform.G2A, "opt68"),
+			new ObjectKeyValuePair(Platform.GOOGLE, "opt1"),
+			new ObjectKeyValuePair(Platform.INSTAGRAM, "opt16"),
+			new ObjectKeyValuePair(Platform.LINKEDIN, "opt8"),
+			new ObjectKeyValuePair(Platform.MAILRU, "opt33"),
+			new ObjectKeyValuePair(Platform.MEETME, "opt17"),
+			new ObjectKeyValuePair(Platform.MICROSOFT, "opt15"),
+			new ObjectKeyValuePair(Platform.NETFLIX, "opt101"),
+			new ObjectKeyValuePair(Platform.OTHER, "opt19"),
+			new ObjectKeyValuePair(Platform.PAYPAL, "opt83"),
+			new ObjectKeyValuePair(Platform.EBAY, "opt83"),
+			new ObjectKeyValuePair(Platform.PROTONMAIL, "opt57"),
+			new ObjectKeyValuePair(Platform.SNAPCHAT, "opt90"),
+			new ObjectKeyValuePair(Platform.SPOTIFY, "opt98"),
+			new ObjectKeyValuePair(Platform.STEAM, "opt58"),
+			new ObjectKeyValuePair(Platform.TELEGRAM, "opt29"),
+			new ObjectKeyValuePair(Platform.TINDER, "opt9"),
+			new ObjectKeyValuePair(Platform.TWILIO, "opt66"),
+			new ObjectKeyValuePair(Platform.TWITTER, "opt41"),
+			new ObjectKeyValuePair(Platform.VIBER, "opt11"),
+			new ObjectKeyValuePair(Platform.WEEBLY, "opt54"),
+			new ObjectKeyValuePair(Platform.WHATSAPP, "opt20"),
+			new ObjectKeyValuePair(Platform.YAHOO, "opt65"),
+			new ObjectKeyValuePair(Platform.YANDEX, "opt23"),
 	}),
-	SMS_ACTIVATE("SMS-Activate", "http://sms-activate.ru/stubs/handler_api.php", false, true, new BasicNameValuePair[] {
-			new BasicNameValuePair(Platform.INSTAGRAM, "ig"),
-			new BasicNameValuePair(Platform.WHATSAPP, "wa"),
-			new BasicNameValuePair(Platform.TELEGRAM, "tg"),
-			new BasicNameValuePair(Platform.OTHER, "ot"),
-			new BasicNameValuePair(Platform.GOOGLE, "go"),
-			new BasicNameValuePair(Platform.FACEBOOK, "fb"),
-			new BasicNameValuePair(Platform.TWITTER, "tw"),
-			new BasicNameValuePair(Platform.MAILRU, "ma"),
-			new BasicNameValuePair(Platform.MICROSOFT, "mm"),
-			new BasicNameValuePair(Platform.YAHOO, "mb"),
-			new BasicNameValuePair(Platform.STEAM, "mt"),
-			new BasicNameValuePair(Platform.TINDER, "oi"),
-			new BasicNameValuePair(Platform.MEETME, "fd"),
-			new BasicNameValuePair(Platform.LINKEDIN, "tn"),
+	SMS_ACTIVATE("SMS-Activate", "http://sms-activate.ru/stubs/handler_api.php", false, true, new ObjectKeyValuePair[] {
+			new ObjectKeyValuePair(Platform.INSTAGRAM, "ig"),
+			new ObjectKeyValuePair(Platform.WHATSAPP, "wa"),
+			new ObjectKeyValuePair(Platform.TELEGRAM, "tg"),
+			new ObjectKeyValuePair(Platform.OTHER, "ot"),
+			new ObjectKeyValuePair(Platform.GOOGLE, "go"),
+			new ObjectKeyValuePair(Platform.FACEBOOK, "fb"),
+			new ObjectKeyValuePair(Platform.TWITTER, "tw"),
+			new ObjectKeyValuePair(Platform.MAILRU, "ma"),
+			new ObjectKeyValuePair(Platform.MICROSOFT, "mm"),
+			new ObjectKeyValuePair(Platform.YAHOO, "mb"),
+			new ObjectKeyValuePair(Platform.STEAM, "mt"),
+			new ObjectKeyValuePair(Platform.TINDER, "oi"),
+			new ObjectKeyValuePair(Platform.MEETME, "fd"),
+			new ObjectKeyValuePair(Platform.LINKEDIN, "tn"),
 	}),
-	NONE("None (manual)", null, false, false, new BasicNameValuePair[] {});
+	NONE("None (manual)", null, false, false, new ObjectKeyValuePair[] {});
 	
 	
 	/**
@@ -128,7 +128,7 @@ public enum SMSProvider {
 	/**
 	 * The available platform codes.
 	 */
-	private final BasicNameValuePair[] platformCodes;
+	private final ObjectKeyValuePair[] platformCodes;
 	
 	
 	/**
@@ -144,7 +144,7 @@ public enum SMSProvider {
 	 * 
 	 * @param platformCodes The available platform codes.
 	 */
-	private SMSProvider(String name, String endpoint, boolean username, boolean apiKey, BasicNameValuePair[] platformCodes) {
+	private SMSProvider(String name, String endpoint, boolean username, boolean apiKey, ObjectKeyValuePair[] platformCodes) {
 		this.name = name;
 		this.endpoint = endpoint;
 		this.username = username;
@@ -196,7 +196,7 @@ public enum SMSProvider {
 	 * @return The platform code.
 	 */
 	public String getPlatformCode(Platform platform) {
-		for (BasicNameValuePair nvp : platformCodes) {
+		for (ObjectKeyValuePair nvp : platformCodes) {
 			if ((Platform)nvp.getKey() == platform) {
 				return nvp.getValueAsString();
 			}
